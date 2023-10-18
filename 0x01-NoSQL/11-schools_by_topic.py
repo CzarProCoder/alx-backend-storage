d77a605eee248e5aad0287a4e297fd939edd18e3
CzarProCoder <juliusczar36@gmail.com>
2023-10-18 16:47:06 +0300

    Update the functoin that returns all the schools containing a particular topic

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -8,5 +8,9 @@ Module containing functin that returns documents that contains a specific topic
 def schools_by_topic(mongo_collection, topic):
     '''
     Function that returns a list of schools that contains a particular topic
+
+    Args:
+        mongo_collection: Collection containing the documents to check
+        topic: Topic to be evaluated
     '''
-    return list(mongo_collection.find({"topic": topic}))
+    return mongo_collection.find({"topic": topic})
