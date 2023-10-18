f69f60d13fba39976eb3da9fee1ff4048f9e85a3
CzarProCoder <juliusczar36@gmail.com>
2023-10-18 16:34:17 +0300

    Create a function that updates the topics of a document within a collection

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+'''
+Module containing function that change topics of a school document
+'''
+
+
+def update_topics(mongo_collection, name, topics):
+    '''
+    Function that updates the name of a document
+
+    Args:
+        Mongo_collection: Collection containing the documents
+        name: Name to be checked
+        topics: Topics to be updated
+    '''
+    mongo_collection.update_many({"name": name}, { "$set": {"topics": topics}})
