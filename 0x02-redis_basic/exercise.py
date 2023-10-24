3d7f2ea1cb48f0434184b7828af21c9529dbc611
CzarProCoder <juliusczar36@gmail.com>
2023-10-24 23:03:19 +0300

    Create decorator to store the history of inputs and outputs for a particular method

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -28,6 +28,25 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    '''
+    Decorator function to record the inputs and outputs
+    of methods
+    '''
+    key = method.__qualname__
+    i = "".join([key, ":inputs"])
+    o = "".join([key, ":outputs"])
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ Wrapper function"""
+        self._redis.rpush(i, str(args))
+        res = method(self, *args, **kwargs)
+        self._redis.rpush(o, str(res))
+        return res
+
+    return wrapper
+
 class Cache:
     '''
     class defining cache created using redis
@@ -40,6 +59,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         '''
         Method that takes data as arguments
