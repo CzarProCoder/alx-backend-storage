8b0094349f1b1c04412d499aa313b1843eeff4b5
CzarProCoder <juliusczar36@gmail.com>
2023-10-24 12:59:25 +0300

    Add a class Cache to allow redis implementation

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,9 +1,30 @@
 #!/usr/bin/env python3
 
 '''
+Module that writes to Redis
 '''
+from uuid import uuid4
+from typing import Union
+import redis
 
 
 class Cache:
+    '''
+    class defining cache created using redis
+    '''
     def __init__(self, ):
-        pass
+        '''
+        Initializes the class Cache
+        '''
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+    
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        '''
+        Method that takes data as arguments
+        
+        Returns a string
+        '''
+        key = str(uuid4())
+        self._redis.mset({key: data})
+        return key
