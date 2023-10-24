3d8da035222faa1c93c9505ab73569d16ef2dec0
CzarProCoder <juliusczar36@gmail.com>
2023-10-24 22:48:58 +0300

    Add a function to count number of times a method has been called

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,8 +5,27 @@ Module that writes to Redis
 '''
 from uuid import uuid4
 from typing import Union, Optional, Callable
+from functools import wraps
 import sys
 import redis
+
+
+def count_calls(method: Callable) -> Callable:
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        Wrap
+        :param self:
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+
+    return wrapper
 
 
 class Cache:
@@ -19,19 +38,20 @@ class Cache:
         '''
         self._redis = redis.Redis()
         self._redis.flushdb()
-    
+
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         '''
         Method that takes data as arguments
-        
+
         Returns a string
         '''
         key = str(uuid4())
         self._redis.mset({key: data})
         return key
 
-    def get(self, key: str, fn: Optional[Callable]  = None) \
-        -> Union[int, float, bytes, str]:
+    def get(self, key: str, fn: Optional[Callable] = None) \
+            -> Union[int, float, bytes, str]:
         '''
         Get method to convert data to desired format
         using the callable function fn
